80deb2b6f544930d031238d3234dd94f10d6bfed
IgiPikek <IgiPikek>
2023-04-16 21:10:29 +0200

    reorder routes by usage

--- server.js
+++ server.js
@@ -86,6 +86,15 @@ SodiumPlus.auto().then(async sodium => {
     const tenant = route => `/:tenantId${route}`;
 
 
+    app.get(tenant(`/`), validateTenant, (req, res) => {
+        console.log(`:tenantId/`, req._hashedTenant);
+
+        const session = Session.newSession();
+
+        // TODO do not render pending tenants. Trying to log into pending tenant throws error because public key doesn't exist yet.
+        res.render(`index`, { tenant: req.params.tenantId, sid: session.id, prod: env.prod });
+    });
+
     app.get(tenant(`/captcha`), requireSession, validateTenant, (req, res) => {
         const captcha = svgCaptcha.createMathExpr({ size: 20, noise: 8, mathMax: 12, mathOperator: `+-` });
         console.log(`captcha`, captcha.text);
@@ -110,6 +119,68 @@ SodiumPlus.auto().then(async sodium => {
 
         res.json(cryptoToken.toString(`hex`));
     });
+
+    app.get(tenant(`/pk`), requireSession, validateTenant, withAuthRest, ({ query, _session, _hashedTenant }, res) => {
+        console.log(query, _session.captcha);
+
+        const publicKeyRaw = require(`.` + path.sep + Dirs.TenantPublicKeyFile(_hashedTenant));
+
+        // this field is probably not strictly necessary, but it might make the distinction between admin and visitors more obvious
+        _session.admin = query.clientPublic === publicKeyRaw;
+
+        console.log(`pk raw`, publicKeyRaw);
+        res.json(publicKeyRaw);
+    });
+
+    app.get(tenant(`/convos/:convoId`), requireSession, validateTenant, withAuthRest, ({ _session, params, _hashedTenant }, res) => {
+        const convoDirs = fs.readdirSync(Dirs.TenantConversations(_hashedTenant));
+
+        if (_session.admin) {
+            const convos = convoDirs.map(convoId => Conversation(
+                convoId,
+                readFilesSync(Dirs.TenantConversation(_hashedTenant, convoId))
+            ));
+            return res.json(convos);
+        }
+
+        const convoId = params.convoId;
+
+        if (!convoDirs.includes(convoId)) {
+            return res.json([Conversation(convoId)]);
+        }
+
+        const messages = readFilesSync(Dirs.TenantConversation(_hashedTenant, convoId));
+
+        const convo = Conversation(convoId, messages);
+
+        console.log(convo);
+        return res.json([convo]);
+    });
+
+    app.post(tenant(`/message`), requireSession, validateTenant, withAuthRest, async ({ body, _hashedTenant }, res) => {
+        console.log(`SUBMIT`);
+
+        // TODO validate
+        const data = body;
+        const convoId = data.k;
+
+        data.t = Date.now();
+        console.log(data);
+
+        const serialized = JSON.stringify(data);
+
+        // TODO perhaps there is a way to encrypt the data once more in order to hide who sent the message (can be seen from the key)
+        // TODO signing or encrypting the data including the timestamp would be interesting in order to avoid manipulation
+
+        const dataHash = (await sodium.crypto_generichash(serialized)).toString(`hex`);
+        console.log(dataHash);
+
+        fs.mkdirSync(Dirs.TenantConversation(_hashedTenant, convoId), { recursive: true });
+        fs.writeFileSync(Dirs.TenantMessage(_hashedTenant, convoId, dataHash), serialized);
+
+        res.send(serialized);
+    });
+
 
     app.get(`/new/:tenant`, async (req, res) => {
         console.log(`/new/:tenant`, req.params.tenant);
@@ -146,79 +217,11 @@ SodiumPlus.auto().then(async sodium => {
         fs.writeFileSync(Dirs.TenantPublicKeyFile(tenantHash), `module.exports = "${req.body.pk}";\n`);
         fs.rmSync(Dirs.PendingTenant(tenantHash));
 
+        // TODO NEXT update in-memory 'tenants' reference
+
         res.send(`/` + tenant);
     });
 
-
-    app.get(tenant(`/`), validateTenant, (req, res) => {
-        console.log(`:tenantId/`, req._hashedTenant);
-
-        const session = Session.newSession();
-
-        // TODO do not render pending tenants. Trying to log into pending tenant throws error because public key doesn't exist yet.
-        res.render(`index`, { tenant: req.params.tenantId, sid: session.id, prod: env.prod });
-    });
-
-    app.get(tenant(`/pk`), requireSession, validateTenant, withAuthRest, ({ query, _session, _hashedTenant }, res) => {
-        console.log(query, _session.captcha);
-
-        const publicKeyRaw = require(`.` + path.sep + Dirs.TenantPublicKeyFile(_hashedTenant));
-
-        // this field is probably not strictly necessary, but it might make the distinction between admin and visitors more obvious
-        _session.admin = query.clientPublic === publicKeyRaw;
-
-        console.log(`pk raw`, publicKeyRaw);
-        res.json(publicKeyRaw);
-    });
-
-    app.post(tenant(`/message`), requireSession, validateTenant, withAuthRest, async ({ body, _hashedTenant }, res) => {
-        console.log(`SUBMIT`);
-
-        // TODO validate
-        const data = body;
-        const convoId = data.k;
-
-        data.t = Date.now();
-        console.log(data);
-
-        const serialized = JSON.stringify(data);
-
-        // TODO perhaps there is a way to encrypt the data once more in order to hide who sent the message (can be seen from the key)
-        // TODO signing or encrypting the data including the timestamp would be interesting in order to avoid manipulation
-
-        const dataHash = (await sodium.crypto_generichash(serialized)).toString(`hex`);
-        console.log(dataHash);
-
-        fs.mkdirSync(Dirs.TenantConversation(_hashedTenant, convoId), { recursive: true });
-        fs.writeFileSync(Dirs.TenantMessage(_hashedTenant, convoId, dataHash), serialized);
-
-        res.send(serialized);
-    });
-
-    app.get(tenant(`/convos/:convoId`), requireSession, validateTenant, withAuthRest, ({ _session, params, _hashedTenant }, res) => {
-        const convoDirs = fs.readdirSync(Dirs.TenantConversations(_hashedTenant));
-
-        if (_session.admin) {
-            const convos = convoDirs.map(convoId => Conversation(
-                convoId,
-                readFilesSync(Dirs.TenantConversation(_hashedTenant, convoId))
-            ));
-            return res.json(convos);
-        }
-
-        const convoId = params.convoId;
-
-        if (!convoDirs.includes(convoId)) {
-            return res.json([Conversation(convoId)]);
-        }
-
-        const messages = readFilesSync(Dirs.TenantConversation(_hashedTenant, convoId));
-
-        const convo = Conversation(convoId, messages);
-
-        console.log(convo);
-        return res.json([convo]);
-    });
 
     function Conversation(id, messages = []) {
         return { id, messages };
